d85c16f4ffc1872bcc705721e90ed276cb1dcfdf
Erica <erica.abdao@zup.com.br>
2025-03-07 11:46:57 -0300

    Chore: primeiro comite com inicialização do projeto pelo spring initializr com as dependencias: Spring segurity, jpa, validation, Lombok, spring web, PostgreSQL e OAuth2.

---
+++ src/main/java/com/desafio/calculoimposto/CalculoimpostoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.calculoimposto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoimpostoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoimpostoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/calculoimposto/CalculoimpostoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.calculoimposto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoimpostoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
