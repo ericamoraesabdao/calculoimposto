8a848099ec238ea62a8e7933808bff5a686d8525
Erica <erica.abdao@zup.com.br>
2025-03-13 19:47:57 -0300

    Feat: Cria a classe CustomUserDatailsService

---
+++ src/main/java/com/desafio/calculoimposto/service/CustomUserDatailsService.java
@@ -0,0 +1,38 @@
+package com.desafio.calculoimposto.service;
+
+import com.desafio.calculoimposto.model.User;
+import com.desafio.calculoimposto.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class CustomUserDatailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username) .orElseThrow(() ->
+                new UsernameNotFoundException("Usuário não encontrado."));
+
+        Set<GrantedAuthority> authorities = user.getRoles().stream()
+                .map((role) -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(
+                username,
+                user.getPassword(),
+                authorities
+        );
+    }
+}
