40955a6b22b5b26e255fc94efd6460790b42c2f8
Erica <erica.abdao@zup.com.br>
2025-03-13 19:57:36 -0300

    Refact: Cria contrato findByName em RoleRepository

--- src/main/java/com/desafio/calculoimposto/repository/RoleRepository.java
+++ src/main/java/com/desafio/calculoimposto/repository/RoleRepository.java
@@ -2,8 +2,13 @@ package com.desafio.calculoimposto.repository;
 
 
 import com.desafio.calculoimposto.model.Role;
+import com.desafio.calculoimposto.model.Roles;
+import com.desafio.calculoimposto.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 
 public interface RoleRepository extends JpaRepository<Role,Long> {
+    Optional<Role> findByName(String name);
 }
