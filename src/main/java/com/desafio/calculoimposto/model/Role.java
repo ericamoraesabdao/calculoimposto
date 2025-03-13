c739d3c97397409c5bc032d8d375d14e40625365
Erica <erica.abdao@zup.com.br>
2025-03-13 19:55:21 -0300

    Refact: Altera o tipo do name da entidade role de String para Enum Roles.

--- src/main/java/com/desafio/calculoimposto/model/Role.java
+++ src/main/java/com/desafio/calculoimposto/model/Role.java
@@ -12,12 +12,15 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private Roles name;
 
-    public Role(String name) {
+    public Role(Roles name) {
         this.name = name;
     }
 
     public Role() {
     }
+
 }
